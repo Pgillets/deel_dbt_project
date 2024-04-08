dd440270e8f2990a974885ae636d8f38dbf1361f
Pgillets <pedro.gillet@gmail.com>
2024-04-07 22:06:13 -0300

    creating dimension and fact tables

--- models/staging/stg_invoices.sql
+++ models/staging/stg_invoices.sql
@@ -1,7 +1,22 @@
 {{
     config(
-        materialized='table'
+        materialized='table',
+        cluster_by=['STATUS','ORGANIZATION_ID']
     )
 }}
 
-select * from {{ source( 'deel','src_csv_invoices' ) }}
+select
+    ORGANIZATION_ID,
+    PARENT_INVOICE_ID,
+    INVOICE_ID,
+    TRANSACTION_ID,
+    TYPE,
+    STATUS,
+    CURRENCY,
+    PAYMENT_CURRENCY,
+    PAYMENT_METHOD,
+    AMOUNT,
+    PAYMENT_AMOUNT,
+    FX_RATE,
+    FX_RATE_PAYMENT
+from {{ source( 'deel','src_csv_invoices' ) }}
--- models/staging/stg_organizations.sql
+++ models/staging/stg_organizations.sql
@@ -4,4 +4,11 @@
     )
 }}
 
-select * from {{ source( 'deel','src_csv_organizations' ) }}
+select
+    ORGANIZATION_ID,
+    FIRST_PAYMENT_DATE,
+    LAST_PAYMENT_DATE,
+    LEGAL_ENTITY_COUNTRY_CODE,
+    COUNT_TOTAL_CONTRACTS_ACTIVE,
+    CREATED_DATE
+from {{ source( 'deel','src_csv_organizations' ) }}
